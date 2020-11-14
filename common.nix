e2dd419bd35be46ea68097ff54a65ebb66a3f812
Mauricio Scheffer <mauricioscheffer@gmail.com>
2020-11-14 15:15:04 +0000

    Upgrade 20.09 channel

--- common.nix
+++ common.nix
@@ -13,8 +13,8 @@ rec {
 
   pkgs2009 = import (builtins.fetchTarball {
     name = "nixpkgs-20.09";
-    url = "https://github.com/NixOS/nixpkgs/archive/d12178b1c4a6ef1232c8c677573ba9db204e66ff.tar.gz";
-    sha256 = "0p7df7yzi35kblxr5ks0rxxp9cfh269g88xpj60sdhdjvfnn6cp7";
+    url = "https://github.com/NixOS/nixpkgs/archive/58f9c4c7d3a42c912362ca68577162e38ea8edfb.tar.gz";
+    sha256 = "1517dy07jf4zhzknqbgm617lgjxsn7a6k1vgq61c67f6h55qs5ij";
   }) {
     config.allowUnfree = true;
     config.permittedInsecurePackages = [
