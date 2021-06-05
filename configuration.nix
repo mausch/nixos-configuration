df2e016d8d01b4d20dcbfb6147e74102e5f24457
Mauricio Scheffer <mauricioscheffer@gmail.com>
2021-06-05 16:51:38 +0100

    Remove some useless comments

--- configuration.nix
+++ configuration.nix
@@ -305,10 +305,10 @@ fonts = {
         ${pkgs.ibus}/bin/ibus-daemon -d &
       '';
       extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock #default i3 screen locker
-        i3blocks #if you are planning on using i3blocks over i3status
+        dmenu 
+        i3status 
+        i3lock 
+        i3blocks 
      ];
     };
   };
