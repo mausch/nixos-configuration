9701255d296d797edd31e1fdeeff315d0c49867b
Mauricio Scheffer <mauricioscheffer@gmail.com>
2021-08-05 09:27:54 +0100

    Add tailscale

--- configuration.nix
+++ configuration.nix
@@ -128,6 +128,8 @@ fonts = {
   # List packages installed in system profile. 
   environment.systemPackages = common.packages ++ (with common.pkgs2105; 
   [
+     tailscale
+
      # gui tools
      gmtp
      xorg.xhost
@@ -219,6 +221,35 @@ fonts = {
   };
   services.openntpd.enable = true;
   services.udisks2.enable = true;
+
+  services.tailscale.enable = true;
+  # ref https://tailscale.com/blog/nixos-minecraft/
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${common.pkgs2105.tailscale}/bin/tailscale status -json | ${common.pkgs2105.jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${common.pkgs2105.tailscale}/bin/tailscale up -authkey ${private.tailscaleKey}
+    '';
+  };
 
   services.udev.extraRules = 
   let 
