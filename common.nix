700f1d52df4815fc288198097d39813663c0674c
Mauricio Scheffer <mauricioscheffer@gmail.com>
2022-08-25 19:13:22 +0100

    Update dotnet

--- common.nix
+++ common.nix
@@ -113,8 +113,9 @@ rec {
      
      (dotnetCorePackages.combinePackages [
         # dotnetCorePackages.sdk_2_1
-        dotnetCorePackages.sdk_3_1 
+        # dotnetCorePackages.sdk_3_1 
         dotnetCorePackages.sdk_5_0
+        dotnetCorePackages.sdk_6_0
      ])
 
      # (import (fetchTarball https://github.com/nix-community/rnix-lsp/archive/23df7ab20b71896ac47da8dab6d4bcc6e8f994d5.tar.gz))
