674aaeec1e1b7a1e2d3cbda895a06364121436cb
Mauricio Scheffer <mauricioscheffer@gmail.com>
2024-11-02 14:36:18 +0000

    Trust cuda-maintainers binary cache

--- common.nix
+++ common.nix
@@ -184,6 +184,9 @@ rec {
           max-jobs = "auto";
           trusted-users = [ "mauricio" ];
         };
+        binaryCachePublicKeys = [
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        ];
         distributedBuilds = true;
         # buildMachines = [
         #   {
