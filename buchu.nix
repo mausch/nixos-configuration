b28192241b84efe9cf504f0f102e91ad400cbd43
Mauricio Scheffer <mauricioscheffer@gmail.com>
2024-12-14 19:23:14 +0000

    Add filebrowser

--- buchu.nix
+++ buchu.nix
@@ -216,6 +216,20 @@ common.recursiveMerge [
   };
   services.prowlarr.enable = true;
 
+  virtualisation.oci-containers.backend = "docker";
+
+  virtualisation.oci-containers.containers.filebrowser = {
+    image = "filebrowser/filebrowser:v2.31.2-s6";
+    volumes = [
+      "/:/srv"
+      "/var/run/filebrowser.db:/database.db"
+      "/var/run/filebrowser.json:/.filebrowser.json"
+    ];
+    ports = [
+      "0.0.0.0:8181:80"
+    ];
+  };
+
   security.polkit.extraConfig =
   ''
     polkit.addRule(function(action, subject) {
