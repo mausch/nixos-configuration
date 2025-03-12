b8af5b65a898c891f67b1cb890e12178d5f4366e
Mauricio Scheffer <mauricioscheffer@gmail.com>
2025-03-12 22:16:02 +0000

    Upgrade openwebui

--- ollama.nix
+++ ollama.nix
@@ -12,7 +12,7 @@
   };
 
   virtualisation.oci-containers.containers.open-webui = {
-    image = "ghcr.io/open-webui/open-webui:v0.4.8";
+    image = "ghcr.io/open-webui/open-webui:v0.5.20";
     volumes = [
       "open-webui:/app/backend/data"
     ];
