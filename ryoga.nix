3cfc220560affac656d8d3a4956c489883657694
Mauricio Scheffer <mauricioscheffer@gmail.com>
2025-06-03 10:42:48 +0100

    Add earlyoom to ryoga

--- ryoga.nix
+++ ryoga.nix
@@ -368,6 +368,10 @@ fonts = {
     '';
   };
 
+  services.earlyoom = {
+    enable = true;
+  };
+
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
