ea0e280a93c59df9928e0f01fe804d1570f40f63
Mauricio Scheffer <mauricioscheffer@gmail.com>
2026-02-01 17:21:27 +0000

    Update open webui

--- ollama.nix
+++ ollama.nix
@@ -12,7 +12,7 @@
   };
 
   virtualisation.oci-containers.containers.open-webui = {
-    image = "ghcr.io/open-webui/open-webui:v0.6.13";
+    image = "ghcr.io/open-webui/open-webui:v0.7.2";
     volumes = [
       "open-webui:/app/backend/data"
     ];
