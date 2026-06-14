ae249775a017658e8a8cc9c900347ad30771a869
Mauricio Scheffer <mauricioscheffer@gmail.com>
2026-06-14 18:51:46 +0100

    Upgrade opencode

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
     nix-lang-server.url = "github:oxalica/nil";
     opencode = {
-      url = "github:anomalyco/opencode/v1.15.1";
+      url = "github:anomalyco/opencode/v1.15.4";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     private = {
